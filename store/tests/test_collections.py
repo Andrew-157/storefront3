4c37ef310ccbd2dda11a736716bdf071f66c0a52
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-15 13:06:39 +0200

    some more tests

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import status
+from model_bakery import baker
 import pytest
+from store.models import Collection, Product
 
 
 @pytest.fixture
@@ -12,20 +14,20 @@ def create_collection(api_client):
 
 @pytest.mark.django_db
 class TestCreateCollection:
-    def test_if_user_is_anonymous_return_401(self, create_collection):
+    def test_if_user_is_anonymous_returns_401(self, create_collection):
 
         response = create_collection({'title': 'a'})
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_if_user_is_not_admin_return_403(self, authenticate, api_client, create_collection):
+    def test_if_user_is_not_admin_returns_403(self, authenticate, api_client, create_collection):
         authenticate()
 
         response = create_collection({'title': 'a'})
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
-    def test_if_data_is_invalid_return_400(self, authenticate, api_client, create_collection):
+    def test_if_data_is_invalid_returns_400(self, authenticate, api_client, create_collection):
         authenticate(is_staff=True)
 
         response = create_collection({'title': ''})
@@ -33,10 +35,26 @@ class TestCreateCollection:
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['title'] is not None
 
-    def test_if_data_is_valid_return_201(self, authenticate, api_client, create_collection):
+    def test_if_data_is_valid_returns_201(self, authenticate, api_client, create_collection):
         authenticate(is_staff=True)
 
         response = create_collection({'title': 'a'})
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data['id'] > 0
+
+
+@pytest.mark.django_db
+class TestRetrieveCollection:
+
+    def test_if_collections_exists_returns_200(self, api_client):
+        collection = baker.make(Collection)
+
+        response = api_client.get(f'/store/collections/{collection.id}/')
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data == {
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0
+        }
