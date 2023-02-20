f6e56d8a319acc024e885dda431c136123fbe088
Andrew Subotin <subotinandrey5@gmail.com>
2023-02-20 11:19:09 +0200

    Logging

--- playground/views.py
+++ playground/views.py
@@ -1,21 +1,16 @@
 from django.core.cache import cache
 from django.shortcuts import render
-from django.views.decorators.cache import cache_page
-from django.utils.decorators import method_decorator
 from rest_framework.views import APIView
+import logging
 import requests
+
+logger = logging.getLogger(__name__)
 
 
 class HelloView(APIView):
-    @method_decorator(cache_page(5 * 10))
     def get(self, request):
+        logger.info('Calling httpbin...')
         response = requests.get('https://httpbin.org/delay/2')
+        logger.info('Received the Response')
         data = response.json()
         return render(request, 'hello.html', {'name': data})
-
-
-# @cache_page(5 * 60)
-# def say_hello(request):
-#     response = requests.get('https://httpbin.org/delay/2')
-#     data = response.json()
-#     return render(request, 'hello.html', {'name': data})
